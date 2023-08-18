7d7cf60902d71d58654c8e4d2075d3d4773190bc
Juan Carlos Alayon Molina <jkam86425@gmail.com>
2023-08-18 08:41:00 -0500

    Sping initializer
    Created Project with Spring Initializer
    Dependencies:
    lombok	                        |    Helps to generate POJO codes
    mysql-connector-java	|    Manages connection to MySQL Database
    spring-boot-starter-web             |    Functionality for web applications created in Spring Boot
    spring-boot-starter-data-jpa      | Java Persistence API, helps handle data access
    spring-boot-starter-thymeleaf   |	Template engine for rendering html pages

---
+++ src/main/java/com/juank648/fleetapp/FleetappApplication.java
@@ -0,0 +1,13 @@
+package com.juank648.fleetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetappApplication.class, args);
+	}
+
+}
