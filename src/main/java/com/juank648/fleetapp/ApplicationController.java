585834fb6cf55c02c244df17441f3c7cba0f2322
Juan Carlos Alayon Molina <jkam86425@gmail.com>
2023-08-23 11:08:41 -0500

    Create ApplicationController.java

---
+++ src/main/java/com/juank648/fleetapp/ApplicationController.java
@@ -0,0 +1,12 @@
+package com.juank648.fleetapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+    @GetMapping("/index")
+    public String goHome() {
+        return "index";
+    }
+}
